27cd2b859f9af0540f01d3a7b5fdfd60b2919b21
Markus Wirtz <info@markuswirtz.net>
2024-11-04 09:31:10 +0100

    Initial Commit

---
+++ NeoneXCTests/NeoneXCTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoneXCTestsApp.swift
+//  NeoneXCTests
+//
+//  Created by Markus Wirtz on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct NeoneXCTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
